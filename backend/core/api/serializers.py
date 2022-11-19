84e55ada95ee1b96d0748206011c7762efe91d9a
Tomdieu <ivantomdio@gmail.com>
2022-11-19 22:56:33 +0100

    updated serializers.py

--- backend/core/api/serializers.py
+++ backend/core/api/serializers.py
@@ -1,19 +1,61 @@
 from rest_framework import serializers
-from  core.models import Account,Transaction
+
+from accounts.api.serializers import UserSerializer
+
+
+from  core.models import Account,TransactionCharge,TransactionType,Transfer,Deposit,Withdraw
 
 class AccountSerializer(serializers.ModelSerializer):
+
+	user = UserSerializer()
 
 	class Meta:
 
 		model = Account
+		fields = '__all__'
+		# fields = ('id','user','balance','is_agent','account_status','currency','created_at','updated_at')
 
-		fields = ('id','user','balance','account_status','currency','created_at','updated_at')
+class DepositSerializer(serializers.ModelSerializer):
 
-
-class TransactionSerializer(serializers.ModelSerializer):
+	deposit_from = AccountSerializer()
+	desposit_to = AccountSerializer()
 
 	class Meta:
 
-		model = Transaction
+		model = Deposit
 
-		fields = ('id','transaction_type','from_account','to_account','date_issue','ammount','created_at')
+		fields = '__all__'
+
+class TransactionTypeSerializer(serializers.ModelSerializer):
+
+	class Meta:
+		model = TransactionType	
+		fields = '__all__'
+
+class TransactionChargeSerializer(serializers.ModelSerializer):
+
+	type = TransactionTypeSerializer()
+
+	class Meta:
+		model = TransactionCharge
+		fields = '__all__'
+
+class Transferserializer(serializers.ModelSerializer):
+
+	sender = AccountSerializer()
+	reciever = AccountSerializer()
+	charge = TransactionChargeSerializer()
+
+	class Meta:
+		model = Transfer
+		fields = '__all__'
+
+class WithdrawSerializer(serializers.ModelSerializer):
+
+	withdraw_from = AccountSerializer()
+	agent= AccountSerializer()
+	withdraw_charge = TransactionChargeSerializer()
+
+	class Meta:
+		model = Withdraw
+		fields = '__all__'
