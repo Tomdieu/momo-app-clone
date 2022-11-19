d6304838bbc3e514ab6c917288271481db358ad4
Tomdieu <ivantomdio@gmail.com>
2022-11-19 22:56:48 +0100

    updated urls.py

--- backend/core/api/urls.py
+++ backend/core/api/urls.py
@@ -1,5 +1,21 @@
 from django.urls import path
+from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('accounts',views.AccountViewSet,basename='accounts')
+router.register('deposits',views.DespositViewSet,basename='deposits')
+router.register('transaction-charges',views.TransactionChargeViewSet,basename='transaction-charge')
+router.register('transaction-types',views.TransactionTypeViewSet,basename='transaction-type'),
+router.register('transfer-money',views.TransferViewSet,basename='send-money')
 
 urlpatterns = [
-	
+	# path('accounts/',views.AccountList.as_view(),name='account'),
+    # path('accounts/<int:id>/',views.AccountDetail.as_view(),name="account-detail"),
+    path('change-pin/',views.ChangePinCode.as_view(),name='change-pin'),
+    # path('transactions/',views.TransactionList.as_view(),name='transaction'),
+    # path('transactions/<int:id>/',views.TransactionDetail.as_view(),name='transaction-detail'),
+    # path('my-transactions/',views.MyTransactions.as_view(),name='my-transactions')
 ]
+
+urlpatterns += router.urls
