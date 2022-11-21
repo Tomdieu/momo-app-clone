5c35a08959dc79dfeae7218aeaeddee0917febc5
Tomdieu <ivantomdio@gmail.com>
2022-11-21 05:32:30 +0100

    updated serializers.py

--- backend/accounts/api/serializers.py
+++ backend/accounts/api/serializers.py
@@ -12,33 +12,44 @@ User = get_user_model()
 
 class LoginSerializer(serializers.Serializer):
 
-    username = serializers.CharField(max_length=120,help_text='username of the account willing to login')
-    password = serializers.CharField(max_length=120,help_text='password of the account willing to login')
+    username = serializers.CharField(
+        max_length=120, help_text='username of the account willing to login')
+    password = serializers.CharField(
+        max_length=120, help_text='password of the account willing to login')
+
 
 class UpdatePasswordSerializer(serializers.Serializer):
 
-    old_password = serializers.CharField(max_length=120,help_text='old password')
-    new_password = serializers.CharField(max_length=120,help_text='new password')
-    confirm_password = serializers.CharField(max_length=120,help_text='confirmation of the new password')
-    
-    
+    old_password = serializers.CharField(
+        max_length=120, help_text='old password')
+    new_password = serializers.CharField(
+        max_length=120, help_text='new password')
+    confirm_password = serializers.CharField(
+        max_length=120, help_text='confirmation of the new password')
+
+    class Meta:
+        extra_kwargs = {'old_password': {'required': True}, 'new_password': {
+            'required': True}, 'confirm_password': {'required': True}}
+
+
 class UserSerializer(serializers.ModelSerializer):
 
     # profile = ProfileSerializer(required=False)
 
     class Meta:
         model = User
-        fields = ['id','username','first_name','last_name','email','password','is_active','is_staff','is_superuser','last_login','date_joined']
-        extra_kwargs = {'password':{
-            'write_only':True,
-            'required':True
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password',
+                  'is_active', 'is_staff', 'is_superuser', 'last_login', 'date_joined']
+        extra_kwargs = {'password': {
+            'write_only': True,
+            'required': True
         },
-        'fist_name':{'required':True},
-        'last_name':{'required':True},
-        'email':{'required':True}
+            'fist_name': {'required': True},
+            'last_name': {'required': True},
+            'email': {'required': True}
         }
-        
-    def create(self,validate_data):
+
+    def create(self, validate_data):
         user = User.objects.create_user(**validate_data)
         user.is_active = True
         user.save()
@@ -46,25 +57,30 @@ class UserSerializer(serializers.ModelSerializer):
         Account.objects.create(user=user)
         return user
 
-    def update_user(self,instance,validated_data):
-        serializer = UserSerializer(instance,data=validated_data,partial=True)
+    def update_user(self, instance, validated_data):
+        serializer = UserSerializer(
+            instance, data=validated_data, partial=True)
         serializer.is_valid(raise_exception=True)
         return serializer.save()
         # return super(UserSerializer,self).update(instance,validated_data)
+
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
 
         model = Profile
-        fields = ['user','phone_number','dob','city','created_at','updated_at']
-    
+        fields = ['user', 'phone_number', 'dob',
+                  'city', 'created_at', 'updated_at']
+
 
 class ProfileListSerializer(WritableNestedModelSerializer):
     user = UserSerializer()
+
     class Meta:
 
         model = Profile
-        fields = ['user','phone_number','dob','city','created_at','updated_at']
+        fields = ['user', 'phone_number', 'dob',
+                  'city', 'created_at', 'updated_at']
 
     def create(self, validated_data):
         user = validated_data.pop('user')
@@ -76,13 +92,13 @@ class ProfileListSerializer(WritableNestedModelSerializer):
 
         return Profile.objects.create(**validated_data)
 
-    def update(self,instance,validated_data):
+    def update(self, instance, validated_data):
 
         nested_serializer = self.fields['user']
         nested_instance = instance.user
 
         nested_data = validated_data.pop('user')
 
-        nested_serializer.update_user(nested_instance,nested_data)
+        nested_serializer.update_user(nested_instance, nested_data)
 
-        return super(ProfileSerializer,self).update(instance,validated_data)
+        return super(ProfileSerializer, self).update(instance, validated_data)
