16ca990a49b1b811d4dcb9a3e0d89e2758062dde
Tomdieu <ivantomdio@gmail.com>
2022-11-22 16:58:55 +0100

    updated urls routes

--- backend/core/api/urls.py
+++ backend/core/api/urls.py
@@ -8,14 +8,9 @@ router.register('deposits',views.DespositViewSet,basename='deposits')
 router.register('transaction-charges',views.TransactionChargeViewSet,basename='transaction-charge')
 router.register('transaction-types',views.TransactionTypeViewSet,basename='transaction-type'),
 router.register('transfer-money',views.TransferViewSet,basename='send-money')
+router.register('change-pin-code',views.ChangePinCodeViewSet,basename='change-pin-code')
 
 urlpatterns = [
-	# path('accounts/',views.AccountList.as_view(),name='account'),
-    # path('accounts/<int:id>/',views.AccountDetail.as_view(),name="account-detail"),
-    path('change-pin/',views.ChangePinCode.as_view(),name='change-pin'),
-    # path('transactions/',views.TransactionList.as_view(),name='transaction'),
-    # path('transactions/<int:id>/',views.TransactionDetail.as_view(),name='transaction-detail'),
-    # path('my-transactions/',views.MyTransactions.as_view(),name='my-transactions')
 ]
 
 urlpatterns += router.urls
