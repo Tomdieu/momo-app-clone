cf5c2250fdce043ac127e5867e60b5a44f90914f
Tomdieu <ivantomdio@gmail.com>
2022-11-26 23:18:56 +0100

    added lang to the Profile attributes

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -6,10 +6,16 @@ User = get_user_model()
 
 class Profile(models.Model):
 
+    LANGUAGES = (
+        ('EN','EN'),
+        ('FR','FR')
+    )
+
     user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
     phone_number = models.CharField(max_length=255,null=True,blank=True,help_text='user phone number') 
     dob = models.DateField(null=True,blank=True,help_text='user date of birth')
     city = models.CharField(max_length=255,blank=True,null=True,help_text='user\'s city ')
+    lang = models.CharField(max_length=2,choices=LANGUAGES,default='EN',help_text='user language')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
