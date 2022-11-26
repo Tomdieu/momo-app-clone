2026154fdda468a079888f64a0d6d1fa68d70c18
Tomdieu <ivantomdio@gmail.com>
2022-11-26 23:21:15 +0100

    added update-

--- backend/accounts/api/urls.py
+++ backend/accounts/api/urls.py
@@ -1,20 +1,22 @@
 from django.urls import path
-from .views import (ProfileList,
+from .views import (ProfileListView,
                     LogoutView, LoginViewSet, ProfileViewSet,
-                    CreateProfileViewSet,UpdatePasswordViewSet)
+                    CreateProfileViewSet,UpdatePasswordViewSet,UpdateLanguage,UserProfileViewSet)
 
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('profile', ProfileViewSet, basename='users')
+router.register('myprofile',UserProfileViewSet,basename='user-profile')
 router.register('create-account', CreateProfileViewSet,
                 basename='create profile')
 router.register('login', LoginViewSet, basename="login")
-router.register('profiles', ProfileList, basename="user-profiles")
+router.register('profiles', ProfileListView, basename="user-profiles")
 router.register('update-password',UpdatePasswordViewSet,basename='updated-password')
+router.register('update-language',UpdateLanguage,basename='language')
 
 urlpatterns = [
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
 
 urlpatterns += router.urls
