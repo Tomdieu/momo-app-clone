53ce803869f3539036345a7a1ca29c10a8cd0ccc
Tomdieu <ivantomdio@gmail.com>
2022-11-27 15:10:42 +0100

    register Notifcation model in admin

--- backend/notifications/admin.py
+++ backend/notifications/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Notification
+
 # Register your models here.
+
+class NotificationAdmin(admin.ModelAdmin):
+
+    list_display = ('user','message','created_at')
+    search_fields = ('user__username','message')
+
+
+admin.site.register(Notification,NotificationAdmin)
