2734965c3373c30b442e1822fd9929960b810f4f
Tomdieu <ivantomdio@gmail.com>
2022-11-27 15:12:52 +0100

    created Notifcation Viewset

--- backend/notifications/views.py
+++ backend/notifications/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import ListModelMixin
+
+from .models import Notification
+from .serializers import NotificationSerializer
 # Create your views here.
+
+
+class NotificationsViewSets(ListModelMixin,GenericViewSet):
+
+    serializer_class = NotificationSerializer
+    permission_class = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Notification.objects.filter(user=self.request.user)
