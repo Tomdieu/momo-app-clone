9d5dcd228cce95d6f0c7b2fc97919699e583b01a
Tomdieu <ivantomdio@gmail.com>
2022-11-27 22:00:23 +0100

    updated urls

--- backend/notifications/urls.py
+++ backend/notifications/urls.py
@@ -1,13 +1,5 @@
-from django.urls import path
-
-from rest_framework.routers import DefaultRouter
-from . import views
-
-router = DefaultRouter()
-router.register('notifications',views.NotificationsViewSets,basename="notifications")
+from django.urls import path,include
 
 urlpatterns = [
-
+    path('notifications/',include('notifications.api.urls'))
 ]
-
-urlpatterns += router.urls
