0dc46970fa9686d17ba11b83188e3fce86446fed
Tomdieu <ivantomdio@gmail.com>
2022-11-27 15:11:07 +0100

    created Notifcation model

--- backend/notifications/models.py
+++ backend/notifications/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
+class Notification(models.Model):
+
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    message = models.TextField(max_length=800)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
