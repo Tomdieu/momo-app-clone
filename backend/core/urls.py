c3437e9fb6b9708129397f3ad53ac90d1682fe4e
Tomdieu <ivantomdio@gmail.com>
2022-11-27 15:04:04 +0100

    remove the withdraw urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,5 +1,16 @@
 from django.urls import path,include
+# from .models import TransactionType,TransactionCharge
 
 urlpatterns = [
 	path('momo/',include('core.api.urls')),
 ]
+
+# This part down here is use to create the different transactiontype 
+
+# T = TransactionType().TRANSACTION_TYPE
+
+# for i in T:
+# 	e = TransactionType.objects.filter(name=i[1])
+# 	if not e.exists():
+# 		t = TransactionType.objects.create(name=i[0],description=f"Descritption of {i[1]}")
+# 		TransactionCharge.objects.create(type=t)
