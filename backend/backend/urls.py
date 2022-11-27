30e287b928cd5909469905ad69b3c838a1c77f25
Tomdieu <ivantomdio@gmail.com>
2022-11-27 15:07:07 +0100

    added notifications urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -44,6 +44,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('accounts.urls'),name='auth'),
     path('api/',include('core.urls'),name='momo'),
+    path('api/',include('notifications.urls')),
     path('api-auth/', include('rest_framework.urls'),name="api-auth"),
     path('api-doc/',include_docs_urls(title='Momo API')),
     path('api/swagger/',swagger_schema_view.with_ui('swagger',cache_timeout=0),name='swagger-schema')
