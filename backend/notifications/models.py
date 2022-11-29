beb8d9a6d5f7a32ac5bdc3afe3ea5bdb58ca82ab
Tomdieu <ivantomdio@gmail.com>
2022-11-29 16:05:13 +0100

    updated

--- backend/notifications/models.py
+++ backend/notifications/models.py
@@ -7,7 +7,25 @@ User = get_user_model()
 
 class Notification(models.Model):
 
+    NOTIFICATION_TYPE = (
+        ('NORMAL','NORMAL'),
+        ('TRANSFER','TRANSFER'),
+        ('TRANSFER_SUCCESSFULL','TRANSFER_SUCCESSFULL'),
+        ('TRANSFER_REJECTED','TRANSFER_REJECTED'),
+        ('WITHDRAW', 'WITHDRAW'),
+        ('WITHDRAW_REJECTED','WITHDRAW_REJECTED'),
+        ('WITHDRAW_CANCEL','WITHDRAW_CANCEL'),
+        ('WITHDRAW_SUCCESSFULL','WITHDRAW_SUCCESSFULL'),
+        ('WITHDRAW_CANCEL', 'WITHDRAW_CANCEL'),
+        ('ACCOUNT_EMPTY','ACCOUNT_EMPTY'),
+        ('ALERT','ALERT')
+    )
+
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     message = models.TextField(max_length=800)
+    type = models.CharField(max_length=20,choices=NOTIFICATION_TYPE)
+
     created_at = models.DateTimeField(auto_now_add=True)
-    
+
+    class Meta:
+        ordering = ['-created_at']
