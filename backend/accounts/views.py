33c1429a0e8f1fbd9f9d305f64c78271020c8309
Tomdieu <ivantomdio@gmail.com>
2022-12-02 08:04:53 +0100

    created langing view

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from datetime import datetime
 
+# from django.views.generic import CreateView,View,TemplateView
+from django.views import View
 # Create your views here.
+
+class LandingPage(View):
+
+    template_name = 'index.html'
+
+
+    def get(self,request,*args,**kwargs):
+
+        context = {
+            'title':'TrixWallet',
+            'date':datetime.now().year
+        }
+        
+        return render(request,self.template_name,context)
