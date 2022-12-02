10ed4eb933c9d60a64a94c51179ca203d9b49a14
Tomdieu <ivantomdio@gmail.com>
2022-12-02 08:02:45 +0100

    updated

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path,include
 
+from . import views
 
 urlpatterns = [
-	path('auth/',include('accounts.api.urls'),name='auth'),
+	# path('auth/',include('accounts.api.urls'),name='auth'),
+	path('',views.LandingPage.as_view(),name='index')
 ]
