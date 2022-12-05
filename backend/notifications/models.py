dddb2f7b3397b80df6fb03bc104a361dec0fb665
Tomdieu <ivantomdio@gmail.com>
2022-12-05 15:47:59 +0100

    updated

--- backend/notifications/models.py
+++ backend/notifications/models.py
@@ -28,7 +28,7 @@ class Notification(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['-created_at']
+        ordering = ['created_at']
 
     def __str__(self):
         return f'{self.user} notification'
