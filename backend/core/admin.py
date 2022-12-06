3562c32711195502ac890fe5d377291eac95ad02
Tomdieu <ivantomdio@gmail.com>
2022-12-06 07:13:24 +0100

    Updated: added admin actions on account

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,20 +1,49 @@
 from django.contrib import admin
+from core.api.utils import converCurrency
+from django.http.response import HttpResponse
+from django.core import serializers
+
+from .api.serializers import AccountSerializer
+from rest_framework.response import Response
 # Register your models here.
 
 from .models import Account,TransactionType,TransactionCharge,Transfer,Withdraw
 
 
+
+
 class AccountAdmin(admin.ModelAdmin):
 
-	list_display = ('id','user','is_agent','amount','account_status','currency')
+	list_display = ('user','is_agent','amount','convertedAmount','account_status','display_currency')
 	list_filter=('balance','account_status')
+	readonly_fields = ('currency','user') 
 
 	search_fields = ('user__username','currency','account_status',)
 	list_per_page = 25
 
-	@admin.display
+	actions = ['to_agent','make_inactive','make_active']
+
+	@admin.display(description="Account Balance",ordering='-created_at')
 	def amount(self,obj):
 		return f'{obj.currency} {obj.balance}'
+
+	@admin.display(description='Balance Converted',ordering='-created_at')
+	def convertedAmount(self,obj:Account):
+		return f'{obj.display_currency} {converCurrency(obj.currency,obj.display_currency,obj.balance)}'
+
+	# Account Actions
+
+	@admin.action(description="Set Account As Agent")
+	def to_agent(self,request,queryset):
+		queryset.update(is_agent=True)
+
+	@admin.action
+	def make_inactive(self, request, queryset):
+		queryset.update(account_status='inactive')
+
+	@admin.action
+	def make_active(self, request, queryset):
+		queryset.update(account_status='active')
 
 
 admin.site.register(Account,AccountAdmin)
@@ -38,8 +67,9 @@ admin.site.register(TransactionCharge,TransactionChargeAdmin)
 
 class TransferAdmin(admin.ModelAdmin):
 	
-	list_display = ('id','code','sender','reciever','transaction_amount','created_at')
+	list_display = ('id','code','sender','reciever','status','transaction_amount','created_at')
 	search_fields = ('code','sender__user__username','reciever__user__username')
+	readonly_fields = ('code','currency')
 
 	@admin.display
 	def transaction_amount(self,obj):
