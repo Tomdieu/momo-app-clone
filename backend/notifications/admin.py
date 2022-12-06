6595502816921572c3bcb8eda7af69c0e2d324b6
Tomdieu <ivantomdio@gmail.com>
2022-12-06 07:15:08 +0100

    Updated

--- backend/notifications/admin.py
+++ backend/notifications/admin.py
@@ -6,8 +6,9 @@ from .models import Notification
 
 class NotificationAdmin(admin.ModelAdmin):
 
-    list_display = ('user','message','created_at')
+    list_display = ('id','user','message','created_at')
     search_fields = ('user__username','message')
+    list_per_page = 25
 
 
 admin.site.register(Notification,NotificationAdmin)
