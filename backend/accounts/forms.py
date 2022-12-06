cd656d847fd99b34fcbc50db58f62fad5b09c8bf
Tomdieu <ivantomdio@gmail.com>
2022-12-06 12:07:14 +0100

    updated

--- backend/accounts/forms.py
+++ backend/accounts/forms.py
@@ -1,65 +1,104 @@
-from django.forms import ModelForm,inlineformset_factory,MultiValueField
+from django.forms import ModelForm
 from django import forms
-from django.core.validators import RegexValidator
 from .models import Profile
+
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 from django.contrib.auth import get_user_model
 
+import datetime
 
 User = get_user_model()
-
-
-class PhoneField(MultiValueField):
-    def __init__(self, **kwargs):
-        # Define one message for all fields.
-        error_messages = {
-            'incomplete': 'Enter a country calling code and a phone number.',
-        }
-        # Or define a different message for each field.
-        fields = (
-            forms.CharField(
-                error_messages={'incomplete': 'Enter a country calling code.'},
-                validators=[
-                    RegexValidator(r'^[0-9]+$', 'Enter a valid country calling code.'),
-                ],
-                help_text='country phone code'
-            ),
-            forms.CharField(
-                error_messages={'incomplete': 'Enter a phone number.'},
-                validators=[RegexValidator(r'^[0-9]+$', 'Enter a valid phone number.')],
-            ),
-            # forms.CharField(
-            #     validators=[RegexValidator(r'^[0-9]+$', 'Enter a valid extension.')],
-            #     required=False,
-            # ),
-        )
-        super().__init__(
-            error_messages=error_messages, fields=fields,
-            require_all_fields=False, **kwargs
-        )
 
 class UserForm(ModelForm):
     
     confirm_password = forms.CharField(max_length=120)
+    last_name = forms.CharField(max_length=120,help_text="last name optional",required=False)
+    first_name = forms.CharField(max_length=120)
     
     class Meta:
 
         model = User
         fields = ['username','first_name','last_name','email','password','confirm_password']
+        required_fields = ['first_name']
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
 
+        if not email:
+            raise ValidationError(_('email required!'))
+
+    # def clean_first_name(self):
+
+    #     first_name = self.cleaned_data.get('first_name')
+
+    #     if not first_name:
+    #         raise ValidationError(_('first name required'))
+
+    #     return first_name
 
     def clean_confirm_password(self):
-        pass
+        confirm_password = self.cleaned_data.get('confirm_password')
+        password= self.cleaned_data.get('password')
+        if confirm_password != password:
+            raise ValidationError( _('The confirm password is different from the password'))
+        # print(self.cleaned_data)
 
+        else:
+            return confirm_password
 
+    def clean_password(self):
+
+        password = self.cleaned_data.get('password')
+
+        if len(password) < 8:
+            raise ValidationError(_('Password length must be >= 8 characters'))
+        else:
+
+            return password
 
 class ProfileForm(ModelForm):
 
     user = UserForm()
-    phone_number = PhoneField()
-    dob = forms.DateField()
     class Meta:
 
         model = Profile
         fields = '__all__'
         exclude = ['created_at','user']
+
+    def clean_dob(self):
+
+        date = self.cleaned_data.get('dob')
+        if not date:
+            raise ValidationError(_("Date of birth required"))
+        if date > datetime.date.today():
+
+            raise ValidationError(_("Invalid date"))
+
+        else:
+            return date
+
+    def clean_city(self):
+
+        city = self.cleaned_data.get('city')
+
+        if not city:
+            raise ValidationError(_('city required'))
+
+        return city
+
+    def clean_phone_number(self):
+
+        phone_number = self.cleaned_data.get('phone_number')
+
+        if not phone_number:
+            raise ValidationError(_('phone number required'))
+
+
+
+class LoginForm(forms.Form):
+
+    username = forms.CharField(max_length=120)
+    password = forms.CharField(widget=forms.PasswordInput)
+
