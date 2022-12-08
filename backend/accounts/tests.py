396633589f14fb58f2890c15fa94625cd0092b8f
Tomdieu <ivantomdio@gmail.com>
2022-12-08 13:46:55 +0100

    updated

--- backend/accounts/tests.py
+++ backend/accounts/tests.py
@@ -1,9 +1,66 @@
-from django.test import TestCase,Client
+from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+
+import json
 
 # Create your tests here.
 
-class AccountsTest(TestCase):
+class AccountTestCase(APITestCase):
 
-    def test2(self):
+    def setUp(self) -> None:
+        self.user = self.create_account()
 
-        self.assertEqual(1+1,2)
+    
+
+    def create_account(self):
+        # token = 'f52ff4c495dff4c05e6f869ef2f3b8250c45c5e2'
+        # self.client.credentials(HTTP_AUTHORIZATION=f"token {token}")
+
+        data = {
+            "user": {
+                "username": "ivantom",
+                "first_name": "navi",
+                "last_name": "gg",
+                "email": "ivantomdio@gmail.com",
+                "password": "1234"
+            },
+            "phone_number": "+222222",
+            "dob": '2000-12-01',
+            "city": "",
+            "lang": 'EN'
+        }
+
+        response = self.client.post('/api/auth/create-account/',data)
+
+        return response
+
+    def authenticate(self):
+        data = {
+            'username':'ivantom',
+            'password':'1234'
+        }
+        response = self.client.post('/api/auth/login/',data)
+        return response
+
+    def fetch_transaction_charges(self):
+        authentication_response = json.loads(self.authenticate().content)
+        token = authentication_response['token']
+
+        self.client.credentials(HTTP_AUTHORIZATION=f"token {token}")
+
+        response = self.client.get('/api/momo/transaction-charges/')
+
+        print(json.loads(response.content))
+
+
+
+
+    def test_1(self):
+        acc = self.create_account()
+        user = json.loads(acc.content)
+        # authentication_response = json.loads(self.authenticate().content)
+
+    def test_2(self):
+        self.fetch_transaction_charges()
+
