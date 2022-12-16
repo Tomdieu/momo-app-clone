3f4437797e72e2099b2ece0986994b1cd5dde846
Tomdieu <ivantomdio@gmail.com>
2022-12-16 20:47:57 +0100

    updated

--- backend/accounts/api/views.py
+++ backend/accounts/api/views.py
@@ -1,9 +1,11 @@
 
-from django.contrib.auth import get_user_model, authenticate, logout,login
+from django.contrib.auth import get_user_model, authenticate, logout, login
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from accounts.api.utils.type import getMethod
 from accounts.models import Profile
 
 from .serializers import (ProfileSerializer, ProfileListSerializer,
@@ -13,6 +15,7 @@ from rest_framework.mixins import (
     CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, ListModelMixin, UpdateModelMixin)
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.generics import CreateAPIView
+
 
 User = get_user_model()
 
@@ -25,7 +28,7 @@ class LogoutView(APIView):
 
         logout(request)
 
-        return Response({'detail': 'You successfully logout'}, status=status.HTTP_200_OK)
+        return Response({'message': 'You successfully logout'}, status=status.HTTP_200_OK)
 
 
 class LoginViewSet(GenericViewSet, CreateAPIView):
@@ -33,15 +36,15 @@ class LoginViewSet(GenericViewSet, CreateAPIView):
     serializer_class = LoginSerializer
 
     def create(self, request, *args, **kwargs):
-        
+
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         username = request.data.get('username')
-        password = request.data.get('password')                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+        password = request.data.get('password')
 
         user = authenticate(username=username, password=password)
         if user is not None:
-            login(request,user)
+            login(request, user)
             return Response({'success': True, 'token': user.auth_token.key})
         else:
             return Response({'success': False, 'error_message': 'username or password incorrect'}, status=status.HTTP_400_BAD_REQUEST)
@@ -53,7 +56,7 @@ class CreateProfileViewSet(CreateModelMixin, GenericViewSet):
 
 
 class ProfileViewSet(ListModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, GenericViewSet):
-    
+
     # serializer_class = ProfileSerializer
     permission_classes = [IsAuthenticated]
 
@@ -65,7 +68,8 @@ class ProfileViewSet(ListModelMixin, RetrieveModelMixin, UpdateModelMixin, Destr
     def get_queryset(self):
         return Profile.objects.filter(user=self.request.user)
 
-class UserProfileViewSet(ListModelMixin,GenericViewSet, RetrieveModelMixin):
+
+class UserProfileViewSet(ListModelMixin, GenericViewSet, RetrieveModelMixin):
 
     serializer_class = ProfileListSerializer
     permission_classes = [IsAuthenticated]
@@ -74,7 +78,7 @@ class UserProfileViewSet(ListModelMixin,GenericViewSet, RetrieveModelMixin):
 
     def get_queryset(self):
         return Profile.objects.all()
-    
+
 
 class UpdatePasswordViewSet(GenericViewSet, CreateAPIView):
 
@@ -91,26 +95,25 @@ class UpdatePasswordViewSet(GenericViewSet, CreateAPIView):
         confirm_password = request.data.get('confirm_password')
 
         if not request.user.check_password(old_password):
-            return Response({'detail':'old password don\'t match'},status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': 'old password don\'t match'}, status=status.HTTP_400_BAD_REQUEST)
 
         if len(new_password) < 8:
-            return Response({'detail':'password is too short require atleast 8 characters'},status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': 'password is too short require atleast 8 characters'}, status=status.HTTP_400_BAD_REQUEST)
 
         if new_password != confirm_password:
-            return Response({'detail':'password don\'t match'},status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': 'password don\'t match'}, status=status.HTTP_400_BAD_REQUEST)
 
-        
         user = request.user
         user.set_password(new_password)
         user.save()
 
         if user.check_password(new_password):
-            return Response({'detail':'password updated successfully'}, status=status.HTTP_200_OK)
+            return Response({'message': 'password updated successfully'}, status=status.HTTP_200_OK)
         else:
-            return Response({'detail':'Something went wrong'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': 'Something went wrong'}, status=status.HTTP_400_BAD_REQUEST)
 
 
-class UpdateLanguage(GenericViewSet,CreateModelMixin):
+class UpdateLanguage(GenericViewSet, CreateModelMixin):
 
     permission_classes = (IsAuthenticated,)
     serializer_class = UserLanguageSerializer
@@ -127,4 +130,19 @@ class UpdateLanguage(GenericViewSet,CreateModelMixin):
         profile.lang = request.data.get('lang')
         profile.save()
 
-        return  Response({'detail':'Language Updated','profile':ProfileListSerializer(profile).data})
+        return Response({'message': 'Language Updated', 'profile': ProfileListSerializer(profile).data})
+
+
+@api_view(['GET'])
+def userExists(request, *args, **kwargs):
+    if getMethod(request):
+        field = kwargs['field']
+        if field == 'username':
+            f = User.objects.filter(username=kwargs['value'])
+        elif field=='email':
+            f = User.objects.filter(email=kwargs['value'])
+
+        if f.exists():
+            return Response({'message': f'a user with this {field} already exists found', 'found': True})
+        else:
+            return Response({'message': f'{field} not found', 'found': False})
