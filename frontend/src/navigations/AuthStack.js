5796166ee1f0656ee3f078725c7163e9b32739ed
Tomdieu <ivantomdio@gmail.com>
2022-12-26 23:27:55 +0100

    Created: AuthStack Navigation

---
+++ frontend/src/navigations/AuthStack.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { createStackNavigator } from '@react-navigation/stack'
+
+import LoginScreen from '../screens/AuthScreens/LoginScreen'
+import WelcomeScreen from '../screens/AuthScreens/WelcomeScreen'
+import RegisterScreen from '../screens/AuthScreens/RegisterScreen'
+
+const Stack = createStackNavigator()
+
+const AuthNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name='Welcome' component={WelcomeScreen} options={{headerShown:false}} />
+        <Stack.Screen name='Login' component={LoginScreen} />
+        <Stack.Screen name='Register' component={RegisterScreen} />
+    </Stack.Navigator>
+  )
+}
+
+export default AuthNavigator
