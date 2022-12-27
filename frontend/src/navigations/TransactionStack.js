ac9516d9d0d49479cfa711a7ccb3622b9c34ef68
Tomdieu <ivantomdio@gmail.com>
2022-12-27 22:11:26 +0100

    updated: Transaction Stack

--- frontend/src/navigations/TransactionStack.js
+++ frontend/src/navigations/TransactionStack.js
@@ -4,6 +4,9 @@ import TransactionScreen from '../screens/TransactionScreens/TransactionScreen'
 import SelectTransactionTypeScreen from '../screens/TransactionScreens/SelectTransactionTypeScreen'
 import ConfirmTransactionScreen from '../screens/TransactionScreens/ConfirmTransactionScreen'
 
+import EnterPhoneNumberScreen from '../screens/TransactionScreens/EnterPhoneNumberScreen'
+import TransactionAmountScreen from '../screens/TransactionScreens/TransactionAmountScreen'
+
 const Stack = createStackNavigator()
 
 
@@ -13,6 +16,8 @@ const TransactionNavigator = ({ navigation,route }) => {
         <Stack.Navigator>
             <Stack.Screen name="Transaction" component={TransactionScreen} />
             <Stack.Screen name='TransactionType' component={SelectTransactionTypeScreen} options={{ headerTitle: 'Transaction options', headerTitleStyle: {textAlign:'center'}}} />
+            <Stack.Screen name="InputPhone" component={EnterPhoneNumberScreen} options={{headerTitle:'Phone Number'}}/>
+            <Stack.Screen name="TransactionAmount" component={TransactionAmountScreen} options={{headerTitle:'Transaction Amount'}}/>
             <Stack.Screen name="ConfirmTransaction" component={ConfirmTransactionScreen} />
 
         </Stack.Navigator>
