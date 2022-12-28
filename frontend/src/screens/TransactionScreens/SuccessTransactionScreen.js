ff92e6e4a0088d7d107d44705c47d684544586d3
Tomdieu <ivantomdio@gmail.com>
2022-12-28 12:24:42 +0100

    Created: Success Transaction Screen

---
+++ frontend/src/screens/TransactionScreens/SuccessTransactionScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SuccessTransactionScreen = () => {
+  return (
+    <View>
+      <Text>SuccessTransactionScreen</Text>
+    </View>
+  )
+}
+
+export default SuccessTransactionScreen
+
+const styles = StyleSheet.create({})
