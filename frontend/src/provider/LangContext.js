63e5bce78a51bf99a1f8eda59bc6cdd7f58eb6f2
Tomdieu <ivantomdio@gmail.com>
2022-12-29 20:17:30 +0100

    Created Language provider

---
+++ frontend/src/provider/LangContext.js
@@ -0,0 +1,33 @@
+import { LangContext } from "../context/LangContext";
+
+import PropTypes from 'prop-types'
+
+// import * as Localization from 'expo-localization'
+
+import {I18n} from "i18n-js";
+
+import LANGUAGES from '../I18n/'
+import { useState } from "react";
+
+export const LangProvider = ({children}) =>{
+
+    const [locale,setLocale]= useState('en')
+
+    const i18n = new I18n(LANGUAGES)
+
+    i18n.fallbacks = true;
+    i18n.defaultLocale = 'en'
+    i18n.locale = locale
+
+
+
+    return (
+        <LangContext.Provider value={{i18n,locale,setLocale}}>
+            {children}
+        </LangContext.Provider>
+    )
+}
+
+LangProvider.propTypes = {
+    children:PropTypes.node.isRequired
+}
