dd4c0a061522eb8c5d3e96323113ce1b61be9e1f
Tomdieu <ivantomdio@gmail.com>
2023-01-07 12:49:26 +0100

    updated settings.py file and commented test database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -106,9 +106,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        "TEST": {
-            "NAME": BASE_DIR / "db_test.sqlite3",
-        },
+        # "TEST": {
+        #     "NAME": BASE_DIR / "db_test.sqlite3",
+        # },
     }
 }
 
