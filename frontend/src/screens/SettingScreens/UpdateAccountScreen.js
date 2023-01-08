17f91d64d3a5a55776abb8869e618fa2a21ec93f
Tomdieu <ivantomdio@gmail.com>
2023-01-08 23:52:05 +0100

    created account updated screen

---
+++ frontend/src/screens/SettingScreens/UpdateAccountScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const UpdateAccountScreen = () => {
+  return (
+    <View>
+      <Text>Update Account</Text>
+    </View>
+  )
+}
+
+export default UpdateAccountScreen
+
+const styles = StyleSheet.create({})
